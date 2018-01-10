244c7fd73e5b6ef68ae565899f05bc9005773ae8
Bob Anderson <rwa@alumni.princeton.edu>
2018-01-09 19:51:02 -0800

    bump version number to 0.15

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='concord232',
-      version='0.14',
+      version='0.15',
       description='GE Concord 4 RS232 Serial Interface Library and Server',
       author='Jason Carter',
       author_email='jason@jason-carter.net',
