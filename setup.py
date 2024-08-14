6166ab171e4c9e7798f849ab72dc44fdd243f30c
Jason Carter <jason@jason-carter.net>
2024-08-14 16:01:56 -0400

    Update setup.py
    Adding License details

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 from setuptools import setup
 
-setup(name='concord232',
-      version='0.15',
-      description='GE Concord 4 RS232 Serial Interface Library and Server',
-      author='Jason Carter',
-      author_email='jason@jason-carter.net',
-      url='http://github.com/JasonCarter80/concord232',
-      packages=['concord232'],
-      install_requires=['requests', 'stevedore', 'prettytable', 'pyserial', 'flask'],
-      scripts=['concord232_server', 'concord232_client']
-  )
+setup(
+    name='concord232',
+    version='0.15',
+    description='GE Concord 4 RS232 Serial Interface Library and Server',
+    author='Jason Carter',
+    author_email='jason@jason-carter.net',
+    url='http://github.com/JasonCarter80/concord232',
+    packages=['concord232'],
+    install_requires=['requests', 'stevedore', 'prettytable', 'pyserial', 'flask'],
+    scripts=['concord232_server', 'concord232_client'],
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ]
+)
