142d2f6ee7b6c44b42baceba3834114d48a32128
Jason Carter <jasoncarter@presidio.com>
2024-08-15 12:39:25 -0400

    Bumping version and adding workflow

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='concord232',
-    version='0.15',
+    version='0.15.1',
     description='GE Concord 4 RS232 Serial Interface Library and Server',
     author='Jason Carter',
     author_email='jason@jason-carter.net',
